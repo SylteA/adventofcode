ae30fd348c1ffb0453bd72c818dfbad7e90758e9
Andreas Sylte <contact@sylte.dev>
2024-12-02 00:53:05 +0100

    Day 1 solution

---
+++ 2024/day1/solution.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+
+def sol1():
+    left, right = [], []
+
+    for line in lines:
+        l, r = map(int, line.split())
+        left.append(l)
+        right.append(r)
+
+    left.sort()
+    right.sort()
+
+    total_distance = 0
+
+    for i in range(len(left)):
+        dist = abs(left[i] - right[i])
+        total_distance += dist
+
+    return total_distance
+
+
+def sol2():
+    left, right = [], defaultdict(lambda: 0)
+
+    for line in lines:
+        l, r = map(int, line.split())
+        left.append(l)
+        right[r] += 1
+
+    left.sort()
+
+    similarity = 0
+
+    for num in left:
+        similarity += num * right[num]
+
+    return similarity
+
+
+if __name__ == '__main__':
+    print(sol2())
