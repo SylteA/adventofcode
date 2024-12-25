66a17242da38f31835448e89c0f70ebb0c73772b
Andreas Sylte <contact@sylte.dev>
2024-12-25 15:05:20 +0100

    day 11 part 2

--- 2024/day11/solution.py
+++ 2024/day11/solution.py
@@ -1,4 +1,5 @@
-from functools import lru_cache
+from collections import defaultdict
+
 
 def apply_rules(stone: int):
     if stone == 0:
@@ -22,24 +23,24 @@ def part1(blinks: int):
     return len(stones)
 
 
+# keeping the order doesn't matter...
 def part2(blinks: int):
-    stones = [*map(int, raw.split()),]
+    stones = {i: 1 for i in map(int, raw.split())}
 
     for _ in range(blinks):
-        ns = [apply_rules(stone) for stone in stones]
-        stones = []
-        for s in ns:
-            stones.extend(s)
-        
-        print(_)
+        tmp = defaultdict(lambda: 0)
+        for num, cnt in stones.items():
+            for new in apply_rules(num):
+                tmp[new] += cnt
 
-    return len(stones)
+        stones = tmp
+
+    return sum(stones.values())
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         raw = f.read()
-    
 
     print(part1(25))
     print(part2(75))
